0650007a74f74dace70b91db1de61155b0b30298
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-08 14:49:50 +0200

    added navbar, _app and a about page

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar';
+
+function App({ Component, pageProps }) {
+    return <>
+        <header>
+            <NavBar />
+        </header>
+        <Component {...pageProps} />
+    </>
+}
+
+export default App;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import NavBar from '../components/NavBar';
+
+function AboutPage() {
+    return <>
+        <main>
+            <h1>About</h1>
+        </main>
+    </>
+}
+
+export default AboutPage;
