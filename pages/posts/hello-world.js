5d96334ed95959e8bc3f39976b821a778e305438
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-08 15:34:06 +0200

    added static props

--- pages/posts/hello-world.js
+++ pages/posts/hello-world.js
@@ -1,18 +1,26 @@
 import Head from "next/head";
 
-function HelloWorldPage() {
+export function getStaticProps() {
+    return {
+        props: {
+            post: {
+                title: "Hello World",
+                content: "This is a first fake entry, so lets just say: Hello World.",
+            }
+        }
+    }
+}
+
+function HelloWorldPage({post}) {
     return <>
         <Head>
             <title>Blog: About</title>
             <meta name="description" content="About page of the blog" />
         </Head>
         <main>
-            <h1>Hello World</h1>
+            <h1>{post.title}</h1>
             <div>
-                <p> This is a first fake entry, so lets just say: Hello World. </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl
-                </p>
+                <p> {post.content}</p>
             </div>
         </main>
     </>
